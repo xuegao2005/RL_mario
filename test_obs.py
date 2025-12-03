0bc4258b3037ee3f2e16ac0e6898709e0b7dac70
xuegao2005 <18915884217xg@gmail.com>
2025-12-03 19:41:51 +0800

    调参

--- test_obs.py
+++ test_obs.py
@@ -9,16 +9,22 @@ from gym.wrappers import GrayScaleObservation, ResizeObservation
 from my_wrapper import SkipFrameWrapper
 
 def make_env():
+
     # 创建SuperMarioBros-v2版本的游戏环境
     env = gym_super_mario_bros.make('SuperMarioBros-v2')
+
     # 使用JoypadSpace包装环境，限制动作空间为SIMPLE_MOVEMENT（仅包含基本移动/跳跃动作）
     env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+    # 应用跳帧包装器（SkipFrameWrapper），每8帧执行一次动作并重复该动作，减少计算量，跳帧可以加快训练/运行速度，同时保留关键游戏状态变化
+    env = SkipFrameWrapper(env, skip=4)
+
     # 灰度图
     env = GrayScaleObservation(env, keep_dim=True)
-    # 应用跳帧包装器（SkipFrameWrapper），每8帧执行一次动作并重复该动作，减少计算量，跳帧可以加快训练/运行速度，同时保留关键游戏状态变化
-    env = SkipFrameWrapper(env, skip=8)
+
     # 压缩像素
     env = ResizeObservation(env, (84, 84))
+
     return env
 
 if __name__ == "__main__":
@@ -26,16 +32,21 @@ if __name__ == "__main__":
 
     # 初始化游戏结束标志为True（表示需要重置环境）
     done = True
+
     # 循环执行4步动作（演示用）
     for step in range(4):
+
         # 如果游戏结束（如马里奥死亡/通关），重置环境获取初始状态
         if done:
             state = env.reset()
+
         # 随机采样一个动作（env.action_space.sample()）并执行，获取新状态、奖励、结束标志、信息
         # 这里使用随机动作仅作演示，实际训练/测试时会用模型预测的动作
         state, reward, done, info = env.step(env.action_space.sample())
+
         # 使用matplotlib显示当前游戏帧（状态），以灰度图形式展示
         plt.imshow(state, cmap='gray')
+
         # 显示图像窗口
         plt.show()
 
--- train_model.py
+++ train_model.py
@@ -12,7 +12,6 @@ import gym_super_mario_bros
 # 导入简化版的马里奥动作空间（减少动作数量，降低训练复杂度）
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from stable_baselines3.common.callbacks import EvalCallback
-
 from test_obs import make_env
 from stable_baselines3.common.vec_env import SubprocVecEnv, VecFrameStack
 
@@ -39,12 +38,14 @@ def main():
     # - verbose=1：打印训练过程中的日志信息
     # - tensorboard_log="logs"：将训练日志保存到logs目录，用于TensorBoard可视化
     model = PPO("CnnPolicy", vec_env, verbose=1, tensorboard_log="logs",
-            learning_rate= 1e-4,
-            n_steps = 2048,
-            batch_size = 8192,
-            n_epochs= 10,
-            ent_coef = 0.1,
-            target_kl=0.1,
+                learning_rate = 3e-4,  # 学习率：模型学得多快，太大容易学乱，太小学得慢
+                n_steps = 2048,  # 每个环境攒2048步数据再更新模型，内存不够就调小这个数
+                batch_size = 2048,  # 每次更新模型时一次喂多少数据，得是（环境数×n_steps）的倍数，不然显存扛不住
+                n_epochs = 10,  # 同样的数据反复学10遍，学透点
+                ent_coef = 0.1,  # 鼓励模型多尝试新动作的程度，越大越爱瞎试
+                target_kl = 0.2,  # 限制模型每次更新别太离谱，超过这个值就停更
+                gamma = 0.97,  # 模型看长远奖励的程度，越接近1越重视以后的奖励
+                clip_range = 0.2,  # 模型更新时的“刹车”，防止步子迈太大导致学崩
     )
 
     # 开始训练模型，总共训练1000个时间步（timesteps）
