cb231ae350a3dfd9f6bcff920b56f6477e7d2ac1
xuegao2005 <18915884217xg@gmail.com>
2025-12-04 09:21:56 +0800

    第二次训练

--- train_model.py
+++ train_model.py
@@ -33,15 +33,15 @@ def main():
 
     model_params = {
 
-        'learning_rate' : 3e-4,  # 学习率：模型学得多快，太大容易学乱，太小学得慢
-        'n_steps' : 2048,  # 每个环境攒2048步数据再更新模型，内存不够就调小这个数
-        'batch_size' : 2048,  # 每次更新模型时一次喂多少数据，得是（环境数×n_steps）的倍数，不然显存扛不住
+        'learning_rate' : 1e-4,  # 学习率：模型学得多快，太大容易学乱，太小学得慢
         'ent_coef' : 0.1,  # 鼓励模型多尝试新动作的程度，越大越爱瞎试
-
-        'target_kl' : 0.2,  # 限制模型每次更新别太离谱，超过这个值就停更
-        'gamma' : 0.97,  # 模型看长远奖励的程度，越接近1越重视以后的奖励
-        'clip_range' : 0.2,  # 模型更新时的“刹车”，防止步子迈太大导致学崩
+        'clip_range': 0.15,  # 模型更新时的“刹车”，防止步子迈太大导致学崩
+        'target_kl' : 0.15,  # 限制模型每次更新别太离谱，超过这个值就停更
         'n_epochs': 10,  # 同样的数据反复学10遍，学透点
+
+        'gamma' : 0.97,  # 模型看长远奖励的程度，越接近1越重视以后的奖励
+        'n_steps': 2048,  # 每个环境攒2048步数据再更新模型，内存不够就调小这个数
+        'batch_size': 2048,  # 每次更新模型时一次喂多少数据，得是（环境数×n_steps）的倍数，不然显存扛不住
 
         # log
         'tensorboard_log' : r'logs', # tensorboard_log="logs"：将训练日志保存到logs目录，用于TensorBoard可视化
@@ -53,10 +53,10 @@ def main():
 
     # 初始化PPO模型
     # - env：绑定的训练环境
-    model = PPO(env = vec_env, **model_params)
+    # model = PPO(env = vec_env, **model_params)
 
     # 读取已有的权重，规定参数，第二轮训练，需要把上面一行注释才能运行！
-    # model = PPO.load('./best_model/best_model.zip', env = vec_env, **model_params)
+    model = PPO.load('./best_model/best_model.zip', env = vec_env, **model_params)
 
 
 
